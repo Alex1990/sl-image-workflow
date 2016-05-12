c78560bd169a9aeb3e80282f98117c6b779a7050
Alex Chao <alexchao1990@gmail.com>
2016-05-13 00:03:41 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/Alex1990/sl-image-workflow/master/sl.sh?token=AC_wLzIq-IVwydcECxv5AbvnQ6-4v0jNks5XPeH-wA%3D%3D -o /usr/local/bin/sl
+
+sudo chmod +x /usr/local/bin/sl
+
+exit 0
