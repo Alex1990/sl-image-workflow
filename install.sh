6643145547bc482730a8a53a9f3d5b575d0e300c
Alex Chao <alexchao1990@gmail.com>
2016-05-24 21:30:10 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/Alex1990/sl-image-workflow/master/sl.sh?token=AC_wLzIq-IVwydcECxv5AbvnQ6-4v0jNks5XPeH-wA%3D%3D -o /usr/local/bin/sl
+curl https://raw.githubusercontent.com/Alex1990/sl-image-workflow/master/sl.sh?token=AC_wL4xJe6gfhYqjqcjXGzn3C2eZm6Tcks5XTZGQwA%3D%3D -o /usr/local/bin/sl
 
-sudo chmod +x /usr/local/bin/sl
+chmod +x /usr/local/bin/sl
 
 exit 0
